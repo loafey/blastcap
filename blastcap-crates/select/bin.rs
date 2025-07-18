acdf7aab5b28d77984de2652a23db44a383aa666
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-07-18 16:00:52 +0200

    Experiment with scopes.

--- blastcap-crates/select/bin.rs
+++ blastcap-crates/select/bin.rs
@@ -1,5 +1,7 @@
 use smol::{future::FutureExt, stream::Stream};
 use std::{
+    cell::RefCell,
+    marker::PhantomData,
     pin::Pin,
     task::{Context, Poll},
 };
@@ -56,4 +58,59 @@ impl<'l, T> Stream for UnorderedInner<'l, T> {
 pub struct Unordered<'l, T> {
     _inner: futures_concurrency::vec::Merge<UnorderedInner<'l, T>>,
 }
-fn main() {}
+
+#[allow(clippy::type_complexity)]
+struct Scope<'env, 'scope, T> {
+    inner: RefCell<Vec<Box<dyn FnMut() -> Pin<Box<dyn Future<Output = T> + 'env>> + 'env>>>,
+    scope: PhantomData<&'scope mut &'scope ()>,
+    env: PhantomData<&'env mut &'env ()>,
+}
+
+impl<'env, 'scope, T> Default for Scope<'env, 'scope, T> {
+    fn default() -> Self {
+        Self {
+            inner: Default::default(),
+            scope: Default::default(),
+            env: Default::default(),
+        }
+    }
+}
+impl<'scope, 'env, T> Scope<'scope, 'env, T> {
+    pub fn add<F, R>(&'env self, mut func: F)
+    where
+        F: FnMut() -> R + 'scope,
+        R: Future<Output = T> + 'scope,
+    {
+        self.inner
+            .borrow_mut()
+            .push(Box::new(move || Box::pin(func())));
+    }
+}
+fn scope<'env, T, F: for<'scope> FnOnce(&'scope Scope<'env, 'scope, T>)>(func: F) {
+    let mut i = Scope::default();
+    func(&mut i);
+}
+
+fn main() {
+    smol::block_on(async {
+        let inc = 0;
+        scope(|s| {
+            s.add(|| async { inc + 1 });
+            s.add(|| async { inc + 2 });
+        });
+        repeat!(async { inc + 1 }, async { inc + 2 });
+        // let mut stream = {
+        //     let mut a = select::select();
+        //     let a_fun = || {
+        //         inc += 1;
+        //         async {}
+        //     };
+        //     a = a.add(a_fun);
+        //     // a = a.add(async || inc += 2);
+        //     a.finish()
+        // };
+        // while let Some(_) = stream.next().await {
+        //     println!("{inc}")
+        // }
+    })
+}
