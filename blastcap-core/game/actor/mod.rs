7fbd962ace524fc765ae1f134b8e1332c32a77ac
Samuel Hammersberg <samuel@loafey.se>
2026-04-12 14:05:50 +0200

    Refactor bots to work with the new system.

--- blastcap-core/game/actor/mod.rs
+++ blastcap-core/game/actor/mod.rs
@@ -44,65 +44,72 @@ impl Actor {
     }
 
     pub async fn bot_act<'l>(&self, state: &ClearRoomState, arg: Arg<'l>) -> anyhow::Result<()> {
-        // let neighs = state
-        //     .get_neighbors(false, self.position)
-        //     .into_iter()
-        //     .filter_map(|(f, p)| match f {
-        //         Piece::Actor(i) if state.actors[i].health > 0 => Some(p),
-        //         _ => None,
-        //     })
-        //     .collect::<Vec<_>>();
-        // if self.resources.abilities == 0 {
-        //     arg.host.mock(ClientRequest::EndTurn).await?;
-        // } else if !neighs.is_empty()
-        // /*&& rand::random_range(0..=1) == 0*/
-        // {
-        //     let pos = neighs[rand::random_range(0..neighs.len())];
-        //     // !TODO!
-        //     // arg.host
-        //     //     .mock(ClientRequest::Action(
-        //     //         "Punch".to_string(),
-        //     //         pos.x,
-        //     //         pos.y,
-        //     //         pos.z,
-        //     //     ))
-        //     //     .await?
-        // } else {
-        //     let others = state
-        //         .actors
-        //         .iter()
-        //         .filter(|a| a.position != self.position && a.health > 0)
-        //         .collect::<Vec<_>>();
-        //     if others.is_empty() {
-        //         arg.host.mock(ClientRequest::EndTurn).await?;
-        //         return Ok(());
-        //     }
-        //     let random = others[rand::random_range(0..others.len())];
-        //     let Some((Vec3 { x, y, z }, _, _)) = state
-        //         .get_neighbors(false, random.position)
-        //         .into_iter()
-        //         .filter(|(a, _)| matches!(a, Piece::Empty))
-        //         .map(|a| a.1)
-        //         .filter_map(|a| state.pathfind(self.position, a).map(|(b, c)| (a, b, c)))
-        //         .min_by_key(|(_, _, c)| *c)
-        //     else {
-        //         arg.host.mock(ClientRequest::EndTurn).await?;
-        //         return Ok(());
-        //     };
-        //
-        //     arg.host
-        //         .mock(ClientRequest::ChatMessage(format!(
-        //             "{} targeting {}",
-        //             self.name, random.name
-        //         )))
-        //         .await?;
-        //
-        //     // !TODO!
-        //     // arg.host
-        //     //     .mock(ClientRequest::Action("Walk".to_string(), x, y, z))
-        //     //     .await?
-        // };
-        arg.host.mock(ClientRequest::EndTurn).await?;
+        let neighs = state
+            .get_neighbors(false, self.position)
+            .into_iter()
+            .filter_map(|(f, p)| match f {
+                Piece::Actor(i) if state.actors[i].health > 0 => Some(p),
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+        if self.resources.abilities == 0 || (self.resources.movement == 0 && neighs.is_empty()) {
+            arg.host.mock(ClientRequest::EndTurn).await?;
+        } else if !neighs.is_empty() {
+            let pos = neighs[rand::random_range(0..neighs.len())];
+            let Some((index, _)) = self
+                .abilities
+                .iter()
+                .enumerate()
+                .filter_map(|(i, id)| DATA.cards.get(id).map(|a| (i, a)))
+                .find(|(_, c)| c.unique_id.as_deref() == Some("card_punch"))
+            else {
+                arg.host.mock(ClientRequest::EndTurn).await?;
+                return Ok(());
+            };
+            arg.host
+                .mock(ClientRequest::Action(index, pos.x, pos.y, pos.z))
+                .await?
+        } else {
+            let others = state
+                .actors
+                .iter()
+                .filter(|a| a.position != self.position && a.health > 0)
+                .collect::<Vec<_>>();
+            if others.is_empty() {
+                arg.host.mock(ClientRequest::EndTurn).await?;
+                return Ok(());
+            }
+            let random = others[rand::random_range(0..others.len())];
+            let Some((Vec3 { x, y, z }, _, _)) = state
+                .get_neighbors(false, random.position)
+                .into_iter()
+                .filter(|(a, _)| matches!(a, Piece::Empty))
+                .map(|a| a.1)
+                .filter_map(|a| state.pathfind(self.position, a).map(|(b, c)| (a, b, c)))
+                .min_by_key(|(_, _, c)| *c)
+            else {
+                arg.host.mock(ClientRequest::EndTurn).await?;
+                return Ok(());
+            };
+            arg.host
+                .mock(ClientRequest::ChatMessage(format!(
+                    "{} targeting {}",
+                    self.name, random.name
+                )))
+                .await?;
+
+            let Some((index, _)) = self
+                .abilities
+                .iter()
+                .enumerate()
+                .filter_map(|(i, id)| DATA.cards.get(id).map(|a| (i, a)))
+                .find(|(_, c)| c.unique_id.as_deref() == Some("card_walk"))
+            else {
+                arg.host.mock(ClientRequest::EndTurn).await?;
+                return Ok(());
+            };
+            arg.host.mock(ClientRequest::Action(index, x, y, z)).await?
+        };
         Ok(())
     }
 }
